c3c1c2aef3cbc6347ce6b9390212485e3c95d95d
nequals30 <nequals30@gmail.com>
2017-04-30 13:44:23 -0500

    minor cleanup

--- yahooFantasy_get_credential.R
+++ yahooFantasy_get_credential.R
@@ -9,13 +9,8 @@ yahooFantasy_get_credential <- function() {
   
   yahoo <- oauth_endpoints("yahoo");
   
-  #yahoo$request <- gsub("oauth/v2", "oauth2", yahoo$request);
-  #yahoo$authorize <- gsub("oauth/v2", "oauth2", yahoo$authorize);
-  #yahoo$access <- gsub("oauth/v2", "oauth2", yahoo$access);
-  
   myapp <- oauth_app("yahoo", key=cKey, secret=cSecret);
-  yahoo_token<- oauth1.0_token(yahoo, myapp, cache=T);
-  #sig <- sign_oauth1.0(myapp, yahoo_token$oauth_token, yahoo_token$oauth_token_secret);
+  yahoo_token<- oauth1.0_token(yahoo, myapp, cache=FALSE);
   
   save(yahoo_token,file="Fantasy.Rdata");
   
