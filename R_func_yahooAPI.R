8b78f792c278b771edb84ac06e3241d6c264ea13
nequals30 <nequals30@gmail.com>
2017-04-30 19:20:13 -0500

    reorganized everything

--- R_func_yahooAPI.R
+++ R_func_yahooAPI.R
@@ -1,17 +1,49 @@
 library(httr)
 library(httpuv)
+library(XML)
 
-yahooFantasy_get_credential <- function() {
+yahooFantasy_get_oauth_token <- function(cKey,cSecret) {
+  # Creates Oauth Token for connecting to Yahoo API and saves it.
+  #
+  # Inputs:
+  #   cKey <- Credential Key (register here: https://developer.yahoo.com/apps/create)
+  #   cSecret <- Credential Secret
+  #
+  # Outputs:
+  #   yahoo_token
   
-  # Gets the yahoo credential and saves it ---------------------------------------------------
-  cKey     <- readLines("myPrivateKey.txt")[1];
-  cSecret  <- readLines("myPrivateKey.txt")[2];
+    yahoo <- oauth_endpoints("yahoo");
+    
+    myapp <- oauth_app("yahoo", key=cKey, secret=cSecret);
+    yahoo_token<- oauth1.0_token(yahoo, myapp, cache=FALSE);
+    
+    save(yahoo_token,file="yahoo_token.Rdata");
+    return(yahoo_token);
+}
+
+yahooFantasy_query <- function(inUrl,yahoo_token){
+  page <-GET(inUrl,config(token=yahoo_token));
+  retrievedXml <- content(page, as="text", encoding="utf-8");
+  outList <- xmlToList(xmlRoot(xmlTreeParse(retrievedXml,useInternal=TRUE)));
+  return(outList)
+}
+
+yahooFantasy_get_gameID <- function(sport,year,yahoo_token){
+  # Ask Yahoo for the 3-digit game_ID based on the sport and season
   
-  yahoo <- oauth_endpoints("yahoo");
+  # hardcode MLB 2017
   
-  myapp <- oauth_app("yahoo", key=cKey, secret=cSecret);
-  yahoo_token<- oauth1.0_token(yahoo, myapp, cache=FALSE);
-  
-  save(yahoo_token,file="Fantasy.Rdata");
-  
+  if (year!=2017){
+    stop("Not implemented. Dont know how to query Yahoo API for season except for current season");
+  }
+  thisUrl <- paste0("http://fantasysports.yahooapis.com/fantasy/v2/game/",sport);
+  thisList <- yahooFantasy_query(thisUrl,yahoo_token);
+  game_ID <- thisList$game$game_id
+  return(game_ID)
+}
+
+yahooFantasy_get_leagueStandings <- function(leagueKey,yahoo_token){
+  thisUrl <- paste0("http://fantasysports.yahooapis.com/fantasy/v2/league/",leagueKey,'/standings');
+  thisList <- yahooFantasy_query(thisUrl,yahoo_token);
+  paste('yo dawg');
 }
